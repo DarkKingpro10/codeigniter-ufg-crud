080fba720ad4359eb372b46289cfd8faf7c66fcf
DarkKingpro10 <jesusgogeta@outlook.com>
2026-01-26 08:37:29 -0600

    fix: Agregar un gitignore para que no intente guardar el debug

--- app/Views/alumnos/edit.php
+++ app/Views/alumnos/edit.php
@@ -1,8 +1,4 @@
 <form action="<?= base_url('alumnos/edit/' . $alumno['id']) ?>" method="post">
-    <input type="text" name="nombre" placeholder="Nombre" value="<?= $alumno['nombre'] ?>">
-    <input type="text" name="apellido" placeholder="Apellido" value="<?= $alumno['apellido'] ?>">
-    <input type="text" name="telefono" placeholder="Teléfono" value="<?= $alumno['telefono'] ?>">
-    <button type="submit">Guardar</button>
     <?= $this->extend('layouts/main') ?>
 
     <?= $this->section('content') ?>
